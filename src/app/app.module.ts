edf1883a82d002a01e755510bd27fb4bcfe81cd9
rails <naloca11@gmail.com>
2018-05-29 13:19:26 -0500

    first app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage, ContactPage,  HomePage, TabsPage } from '../pages/index.paginas';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -33,6 +31,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   providers: [
     StatusBar,
+    BarcodeScanner,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
---
+++ src/pages/index.paginas.ts
@@ -0,0 +1,4 @@
+export { HomePage } from './home/home';
+export { AboutPage } from './about/about';
+export { ContactPage } from './contact/contact';
+export { TabsPage } from './tabs/tabs';
